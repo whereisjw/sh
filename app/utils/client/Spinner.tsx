e2b83c3fce21aaf445148911b2ebe21d18d77fba
whereisjw <j9399977@gmail.com>
2024-01-26 20:27:25 +0900

    /api/products 업로드 api생성

---
+++ app/utils/client/Spinner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+ <>  <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+  
+</svg></>
+  )
+}
+
+export default Spinner
---
+++ pages/api/products.tsx
@@ -0,0 +1,34 @@
+import smtpTransport from "@/prisma/email";
+import prisma from "../../prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withIronSessionApiRoute } from "iron-session/next";
+
+ 
+interface IResponse{
+    ok?:boolean;
+    [key:string]:any;
+}
+
+export default withIronSessionApiRoute( async function handler(req:NextApiRequest,res:NextApiResponse<IResponse>){
+   const {name,price,description} =req.body;
+   const {user} = req.session;
+if(req.method === 'POST'){
+    const products = await prisma.product.create({
+    data:{
+        name,
+        price,
+        description,
+        image:'1',
+        user:{
+            connect:{
+                id:user?.id,
+            }
+        }
+    }
+    }) 
+    res.json({ok:true,products})
+}//GET
+},{
+ cookieName:"shSession",
+ password:"1354948945415616571651765156741675174657414765156489649949"   
+})
