a2d7b6fa3689f0fe2f1f308476234712b37757c8
whereisjw <j9399977@gmail.com>
2024-02-19 16:43:38 +0900

    메일 재수정

--- pages/api/users/login.tsx
+++ pages/api/users/login.tsx
@@ -41,22 +41,20 @@ export default async function handler(
         subject: "하마마켓 1회용 인증메일입니다.",
         text: `Authentication Code : ${암호}`,
       };
-      const result = await smtpTransport.sendMail(
-        mailOptions,
-        (error, responses) => {
-          if (error) {
-            console.log(error);
-            return null;
+      await new Promise((resolve, reject) => {
+        // send mail
+        smtpTransport.sendMail(mailOptions, (err, info) => {
+          if (err) {
+            console.error(err);
+            reject(err);
           } else {
-            console.log(responses);
-            return null;
+            console.log(info);
+            resolve(info);
           }
-        }
-      );
+        });
+      });
       smtpTransport.close();
-      console.log(result);
     }
-    console.log("하하");
 
     res.status(200).json({ ok: true });
   } //post
